d12b0ef21d4ced0635ac556e2ff1f6ead52f44cd
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 00:37:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
   end
   
   def self.all_students_in_grade_9
-    sql = "SELECT * FROM students WHERE grade = ?"
+    sql = "SELECT * FROM students WHERE grade = '9'"
     DB[:conn].execute(sql)
     end
   end
