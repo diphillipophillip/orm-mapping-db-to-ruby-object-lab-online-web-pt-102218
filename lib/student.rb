7fcc33b7d35a682a5ba6ea4e28d0a93d0ef0693d
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 01:21:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
     sql = "SELECT * FROM students WHERE grade <= 11"
     row = DB[:conn].execute(sql).flatten
     sam = self.new_from_db(row)
-    sam.name
+    binding.pry
   end
 
   
