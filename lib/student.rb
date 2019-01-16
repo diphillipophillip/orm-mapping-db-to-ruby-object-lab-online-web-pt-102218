4e0241787955060783dc9296981d5af62cce648c
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 01:16:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
   end
   
   def self.students_below_12th_grade 
-    sql = "SELECT * FROM students WHERE grade BETWEEN 1 AND 11"
+    sql = "SELECT * FROM students WHERE grade BETWEEN 1 AND 11 GROUP BY name"
     this = DB[:conn].execute(sql).flatten
   end
 
