23709512a03c4fc432894260c7d0ec293605a9e3
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 01:17:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,8 @@ class Student
   def self.students_below_12th_grade 
     sql = "SELECT * FROM students WHERE grade <= 11"
     this = DB[:conn].execute(sql).flatten
-    this.pop
+    that = this.pop
+    that.pop
   end
 
   
