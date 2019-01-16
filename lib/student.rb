74f69fa98934b93404c3bd914a0026874df87596
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 00:22:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,9 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    binding.pry
   end
 
   def self.all
