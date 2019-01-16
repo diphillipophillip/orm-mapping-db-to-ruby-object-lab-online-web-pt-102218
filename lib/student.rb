85a62f6963c5f520c6666a649acb0e3388346f8e
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 01:38:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,11 +38,7 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(number)
-    sql = "SELECT * FROM students WHERE grade = 10"
-    DB[:conn].execute(sql, number).map do |i| 
-      self.new_from_db(i) 
-      binding.pry
-    end
+  
   end
 
   
