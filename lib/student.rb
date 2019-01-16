10fed6ab87313aa515e46aef4e209484e568a124
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 00:38:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,11 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.students_below_12th_grade 
+    sql = "SELECT * FROM students WHERE grade < '12'"
+    DB[:conn].execute(sql)
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
