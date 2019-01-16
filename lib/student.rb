12ded87ff40f09d44a12f2da556cb567b38cbb9b
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 00:31:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |i| 
   end
   
   def save
