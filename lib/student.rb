88ae01fa3ece08a7db40d53cfb6e59d555496af2
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-16 00:42:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,8 +30,10 @@ class Student
   end
   
   def self.students_below_12th_grade 
+    arr = []
     sql = "SELECT * FROM students WHERE grade < '12'"
-    DB[:conn].execute(sql).size
+    arr << DB[:conn].execute(sql)
+    binding.pry
   end
   
   def save
